ae43c09218b2b6a0c78ac21c11b2669008e232cc
Shayan <biffgarfield21@gmail.com>
2023-09-26 14:30:56 +0530

    Signup page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './screens/Home';
 import React from "react";
+import Signup from './screens/Signup';
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,6 +19,7 @@ function App() {
      <Routes>
       <Route exact path="/" element={<Home/>} />
       <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/createuser" element={<Signup/>} />
       
      </Routes>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,10 +15,14 @@ export default function Navbar() {
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
+        <ul className="navbar-nav">
         <li className="nav-item">
           <Link className="nav-link" to="/login">Login</Link>
         </li>
-
+        <li className="nav-item">
+          <Link className="nav-link" to="/createuser">Signup</Link>
+        </li>
+        </ul>
       </ul>
     </div>
   </div>
