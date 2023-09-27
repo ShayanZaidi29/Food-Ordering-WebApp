dce27584db58fd285bdd5073e72ddb15d2d6dc52
Shayan <biffgarfield21@gmail.com>
2023-09-27 13:44:50 +0530

    Fetch issue fixed

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -11,73 +11,86 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = fetch("http://localhost:5000/api/createuser", {
-      method: "POST",
-      header: {
-        "Content-Type": "application/json",
+    const response = await fetch("http://localhost:5000/api/createuser", {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(),
-    });
-  };
+      body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation })
 
-  const onChange=(event)=>{
-    setcredentials({...credentials,[event.target.name]:event.target.value})
+    });
+    const json = await response.json()
+    console.log(json);
+    if (json.success) {
+      //save the auth toke to local storage and redirect
+      localStorage.setItem('token', json.authToken)
+      // navigate("/login")
+
+    }
+    else {
+      alert("Enter Valid Credentials")
+    }
   }
 
+  const onChange = (event) => {
+    setcredentials({ ...credentials, [event.target.name]: event.target.value });
+  };
 
   return (
     <>
       <div className="container">
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Name
             </label>
             <input
               type="name"
               className="form-control"
-              name='name'
+              name="name"
               value={credentials.name}
-              onChange={onChange}              
+              onChange={onChange}
             />
           </div>
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
               type="email"
               className="form-control"
-              name='email'
+              name="email"
               value={credentials.email}
               onChange={onChange}
-              />
+            />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
             </div>
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
               className="form-control"
               id="exampleInputPassword1"
-              name='password'
+              name="password"
               value={credentials.password}
               onChange={onChange}
             />
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Address
             </label>
             <input
-              type="password"
+              type="text"
               className="form-control"
               id="exampleInputPassword1"
-              name='geolocation'
+              name="geolocation"
               value={credentials.geolocation}
               onChange={onChange}
             />
