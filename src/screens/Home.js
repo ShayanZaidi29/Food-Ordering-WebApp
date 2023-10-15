3fc20a552b480691ca5a33372400a5188cddeef7
Shayan <biffgarfield21@gmail.com>
2023-10-15 13:23:53 +0530

    Category displayed on website

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import Carousal from "../components/Carousal";
 
 export default function Home() {
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    try {
+      const foodCatResponse = await fetch("http://localhost:5000/api/foodCategory", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const foodItemResponse = await fetch("http://localhost:5000/api/foodData", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const foodCatData = await foodCatResponse.json();
+      const foodItemData = await foodItemResponse.json();
+
+      setFoodCat(foodCatData);
+      setFoodItem(foodItemData);
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div>
-      <div><Navbar /></div>
-      <div><Carousal/></div>
-      <div className="m-3"></div>
-
-      <div>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <Navbar />
+      <Carousal />
+      <div className="container">
+        {foodCat.length > 0
+          ? foodCat.map((data) => <div key={data.id}>{data.CategoryName}</div>)
+          : <div>No data available</div>}
+        <Card />
       </div>
-
-
-
-
-      <div><Footer /></div>
+      <Footer />
     </div>
   );
 }
