1b1812a40c966559d101b1e226cf5e936650f30c
Shayan <biffgarfield21@gmail.com>
2023-10-20 21:06:05 +0530

    Add to cart fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,12 +6,25 @@ export default function Card(props) {
   const data = useCart();
   const navigate = useNavigate();
   const [qty, setQty] = useState(1);
-  const [size, setSize] = useState('');
+  const [size, setSize] = useState("");
   const priceRef = useRef();
   const options = props.options;
-  const priceOptions = Object.keys(options);
-  const foodItem = props.foodItem; // Corrected variable name
+  const foodItem = props.foodItem;
   const dispatch = useDispatchCart();
+
+  const handleAddToCart = async () => {
+    const finalPrice = qty * parseInt(options[size]);
+
+    await dispatch({
+      type: 'ADD',
+      id: foodItem._id,
+      name: foodItem.name,
+      price: finalPrice,
+      qty: qty,
+      size: size,
+    });
+    console.log(data);
+  };
 
   const handleClick = () => {
     if (!localStorage.getItem('token')) {
@@ -20,46 +33,18 @@ export default function Card(props) {
   };
 
   const handleQty = (e) => {
-    setQty(e.target.value);
+    setQty(parseInt(e.target.value)); // Parse the value to an integer
   };
 
   const handleOptions = (e) => {
     setSize(e.target.value);
   };
 
+  let finalPrice = qty * parseInt(options[size]);
+
   useEffect(() => {
-    setSize(priceRef.current.value);
-  }, []);
-
-  const handleAddToCart = async () => {
-    if (!foodItem || !foodItem._id) {
-      console.error('Invalid food item or missing _id property');
-      return;
-    }
-
-    const finalPrice = qty * parseInt(options[size]);
-
-    const existingFood = data.find((item) => item.id === foodItem._id);
-
-    if (existingFood) {
-      if (existingFood.size === size) {
-        await dispatch({ type: 'UPDATE', id: foodItem._id, price: finalPrice, qty: qty });
-      } else {
-        await dispatch({
-          type: 'ADD',
-          id: foodItem._id,
-          name: foodItem.name,
-          price: finalPrice,
-          qty: qty,
-          size: size,
-          img: props.ImgSrc,
-        });
-        console.log('Size different, so adding one more to the list');
-      }
-    } else {
-      await dispatch({ type: 'ADD', id: foodItem._id, name: foodItem.name, price: finalPrice, qty: qty, size: size });
-    }
-  };
+    setSize(priceRef.current.value)
+  }, [])
 
   return (
     <div>
@@ -68,21 +53,25 @@ export default function Card(props) {
         <div className="card-body">
           <h5 className="card-title">{foodItem.name}</h5>
           <div className="container w-100 p-0" style={{ height: '38px' }}>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" onClick={handleClick} onChange={handleQty}>
+            <select className="m-2 h-100 w-20 bg-success text-black rounded" onChange={handleQty}>
               {Array.from(Array(6), (e, i) => (
                 <option key={i + 1} value={i + 1}>
                   {i + 1}
                 </option>
               ))}
             </select>
-            <select className="m-2 h-100 w-20 bg-success text-black rounded" ref={priceRef} onClick={handleClick} onChange={handleOptions}>
-              {priceOptions.map((i) => (
-                <option key={i} value={i}>
-                  {i}
+            <select className="m-2 h-100 w-20 bg-success rounded" ref={priceRef} onChange={(e)=> setSize(e.target.value)}>
+              {Object.keys(options).map((option) => (
+                <option key={option} value={option}>
+                  {option}
                 </option>
               ))}
+
+              
             </select>
-            <div className="d-inline ms-2 h-100 w-20 fs-5">₹{qty * parseInt(options[size])}/-</div>
+            <div className="d-inline ms-2 h-100 w-20 fs-5">
+              ₹{finalPrice}/-
+              </div>
           </div>
           <hr></hr>
           <button className="btn btn-success justify-center ms-2" onClick={handleAddToCart}>
