9a07e3049e34c46225c812083d12c38aca0e608f
Shayan <biffgarfield21@gmail.com>
2023-10-21 18:52:36 +0530

    Cart added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Badge from 'react-bootstrap/Badge'; // Corrected import statement
+import Modal from '../Modal';
+import Cart from '../screens/Cart';
+
 
 export default function Navbar() {
+  const [cartView, setCartView] = useState(false)
   const [isLoggedIn] = useState(!!localStorage.getItem("authToken")); // Check if the user is logged in
 
   const navigate = useNavigate();
@@ -49,7 +54,11 @@ export default function Navbar() {
               )}
             </ul>
             <div>
-              {isLoggedIn && <div className='btn bg-white text-success mx-2'>My Cart</div>}
+              {isLoggedIn && <div className='btn bg-white text-success mx-2' onClick={()=> {setCartView(true)}}>
+                My Cart {" "}
+                <Badge pill bg="danger" > 2 </Badge>
+                </div>}
+                {cartView? <Modal onClose={()=> setCartView(false)} ><Cart/></Modal>:null}
             </div>
             <div className='d-flex'>
               {isLoggedIn ? (
